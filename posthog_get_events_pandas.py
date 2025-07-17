bc6f1af293d3ba7e2f4964c9c8b644bed3ea0899
Michael White <hrt666.cs@gmail.com>
2025-07-17 10:50:18 +0900

    Added POSTHOG_FILTER_EVENT in .env

--- posthog_get_events_pandas.py
+++ posthog_get_events_pandas.py
@@ -9,6 +9,7 @@ api_key = os.getenv("POSTHOG_PERSONAL_API_KEY")
 host = os.getenv("POSTHOG_HOST")
 project_id = os.getenv("POSTHOG_PROJECT_ID")
 url = f"{host}/api/projects/{project_id}/events"
+filter_event = os.getenv("POSTHOG_FILTER_EVENT")
 
 if not all([api_key, project_id, host]):
     raise ValueError("Required .env info (API KEY, Project ID or host) are missing")
@@ -18,16 +19,16 @@ headers = {
 }
 
 params = {
-    "limit": 10  # the latest data
+    "limit": 10  # latest 10 events
 }
 
 response = requests.get(url, headers=headers, params=params)
 events = response.json().get("results", [])
 
-filtered = [e for e in events if e["event"] == "test_event"]
+filtered = [e for e in events if e["event"] == filter_event]
 
 df = pd.DataFrame(filtered)
 if not df.empty:
     print(df[["timestamp", "distinct_id", "event"]])
 else:
-    print("DataFrame is empty.")
+    print("DataFrame is empty.: {filter_event}")
